2c967612f5b9ca2e0a20fee2875c683c3ee9c3bc
Murilo Martins <murilomartins@outlook.com>
2022-10-27 11:29:34 -0300

    :books: GET clients

--- src/main/java/com/murilomartins/clientcrud/services/ClientService.java
+++ src/main/java/com/murilomartins/clientcrud/services/ClientService.java
@@ -1,6 +1,9 @@
 package com.murilomartins.clientcrud.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +23,12 @@ public class ClientService {
 	public ClientDTO findByID(Long id) {
 		Client client = repository.findById(id).orElseThrow(() -> new RuntimeException("Resource not found"));
 		return new ClientDTO(client);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
 	}
 	
 }
