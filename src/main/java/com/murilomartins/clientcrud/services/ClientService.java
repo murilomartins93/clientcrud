876f0ca09fae31366a56f3a98b631c3d867f9480
Murilo Martins <murilomartins@outlook.com>
2022-10-27 15:59:22 -0300

    :books: Exception handling

--- src/main/java/com/murilomartins/clientcrud/services/ClientService.java
+++ src/main/java/com/murilomartins/clientcrud/services/ClientService.java
@@ -1,39 +1,41 @@
 package com.murilomartins.clientcrud.services;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.murilomartins.clientcrud.dto.ClientDTO;
 import com.murilomartins.clientcrud.entities.Client;
 import com.murilomartins.clientcrud.repositories.ClientRepository;
+import com.murilomartins.clientcrud.services.exceptions.ResourceNotFoundException;
 
-
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findByID(Long id) {
-		Client client = repository.findById(id).orElseThrow(() -> new RuntimeException("Resource not found"));
+		Client client = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
 		return new ClientDTO(client);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(Pageable pageable) {
 		Page<Client> result = repository.findAll(pageable);
 		return result.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
-		Client client = new Client(); 
+		Client client = new Client();
 		copyDtoToEntity(dto, client);
 		client = repository.save(client);
 		return new ClientDTO(client);
@@ -41,17 +43,26 @@ public class ClientService {
 
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) {
-		Client client = repository.getReferenceById(id);
-		copyDtoToEntity(dto, client);
-		client = repository.save(client);
-		return new ClientDTO(client);
+		try {
+			Client client = repository.getReferenceById(id);
+			copyDtoToEntity(dto, client);
+			client = repository.save(client);
+			return new ClientDTO(client);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Resource not found");
+		}
 	}
-	
-	@Transactional
+
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Resource not found");
+		}
 	}
-	
+
 	private void copyDtoToEntity(ClientDTO dto, Client client) {
 		client.setName(dto.getName());
 		client.setCpf(dto.getCpf());
@@ -59,5 +70,5 @@ public class ClientService {
 		client.setBirthDate(dto.getBirthDate());
 		client.setChildren(dto.getChildren());
 	}
-	
+
 }
